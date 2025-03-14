eda3ea49da8834977bd470addf8433726eed1b96
Humaira Tasnim Adiba <123799736+adibaruet@users.noreply.github.com>
2025-03-14 19:26:05 +0600

    Update main..go

--- A Crud Server/main..go
+++ A Crud Server/main..go
@@ -1,1 +1,23 @@
-aa
+package main 
+import(
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct{
+	ID string 'json':"id"
+	Isbn string 'json:"isbn"'
+	Title string 'json:"title"'
+	Director *Director 'json:"director"'
+
+}
+
+type Director struct{
+     Firstname string 'json:"firstname"'
+	 Lastname string 'json:"lasttname"'
+}
